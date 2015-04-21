984a055be399ac06b3c84b13cee44a00aa9a9980
Alumno <Alumno@10.206.97.42>
2015-04-21 15:04:56 +0200

    Declaracion de las estructuras

--- proyectoCaracola/src/nodo.h
+++ proyectoCaracola/src/nodo.h
@@ -8,4 +8,29 @@ char opcion();//para introdicir la eleccion
 //registro
 void enunciado2();//listado de las opciones del registro
 void registro();//para introducir los datos
+char opcion2Nombre();
+
+typedef struct{
+	char dni;
+	char nombre;
+	char apellido;
+	char telefono;
+}Cliente;
+
+typedef struct{
+	char dni;
+	char nombre;
+	char apellido;
+	char exp;
+	char telefono;
+
+
+}Profesor;
+
+typedef struct{
+	char matricula;
+	char tipo;
+	char color;
+
+}Coche;
 #endif
